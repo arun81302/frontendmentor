f00ad1418f5a364d2b0a575a42253bba707a3333
Arun <arunsingh81302@gmail.com>
2026-07-28 19:43:52 +0530

    prject done desktop design done mobile design pending

--- article-preview-component-master/script.js
+++ article-preview-component-master/script.js
@@ -1,16 +1,7 @@
-let after_share=document.querySelector('.bottom-share')
-let main=document.querySelector('.bottom-avatar-section')
-let main_icon=document.querySelector('.icon-image')
-let after_icon=document.querySelector('.icon')
-console.log(after_share)
-console.log(main)
-console.log(main_icon)
-console.log(after_icon)
-main_icon.addEventListener('click',()=>{
-    after_share.style.display='flex'
-    main.style.display='none'
-})
-after_icon.addEventListener('click',()=>{
-        after_share.style.display='none'
-    main.style.display='flex'
+const shareBtn=document.querySelector('.share-cont')
+const wrap=document.querySelector('.wrap')
+console.log(shareBtn)
+
+shareBtn.addEventListener('click',()=>{
+wrap.classList.toggle('hide')
 })
