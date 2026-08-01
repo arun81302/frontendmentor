ff36d8c9ed89f2a739c8e7bacd60525503c0152e
Arun <arunsingh81302@gmail.com>
2026-08-01 18:26:06 +0530

    article preview component project done

--- article-preview-component-master/script.js
+++ article-preview-component-master/script.js
@@ -1,7 +1,21 @@
 const shareBtn=document.querySelector('.share-cont')
 const wrap=document.querySelector('.wrap')
-console.log(shareBtn)
+const mobile_wrap=document.querySelector('.mobile-share')
+const user=document.querySelector('.user')
+const mobileBtn=document.querySelector('.mobile-share-icon-btn')
+// console.log(shareBtn)
 
 shareBtn.addEventListener('click',()=>{
-wrap.classList.toggle('hide')
+    if(window.innerWidth<=767){
+        mobile_wrap.classList.toggle('mobile-hide')
+        user.classList.toggle('mobile-hide')
+
+    }else{
+        wrap.classList.toggle('hide')
+    }
+
+})
+mobileBtn.addEventListener("click", () => {
+    mobile_wrap.classList.toggle('mobile-hide')
+        user.classList.toggle('mobile-hide')
 })
